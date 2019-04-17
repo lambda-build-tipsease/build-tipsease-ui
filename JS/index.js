22806cac05a658737342c2c9d71f0938d89cde80
Manya0 <manya01@protonmail.com>
2019-04-17 13:36:12 -0400

    some-changes

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-let navigation = document.querySelectorAll('.nav a');
+let navigation = document.querySelectorAll('a');
 
 for (i = 0; i < navigation.length; i++) {
     navigation[i].addEventListener('mouseover', function(event) {
@@ -8,3 +8,19 @@ for (i = 0; i < navigation.length; i++) {
       event.target.style.transform = 'scale(1)';
     })        
 }
+let logo = document.getElementsByTagName('h3');
+logo.addEventListener('mouseover', function(event) {
+    event.target.style.transform = 'scale(3)';
+});
+logo.addEventListener('mouseout', (event) => event.target.style.transform = 'scale(1)');
+// class Name{
+//     constructor(name) {
+//         this.name = name;
+//         this.expand = this.name.querySelector('')
+//     }
+// }
+
+// let names = document.querySelectorAll('.name')
+// names.forEach(name => {
+//     new Name(name)
+// });
