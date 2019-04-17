a8ce077b8f234980aa18dcefc11500b60967e09e
Manya0 <manya01@protonmail.com>
2019-04-17 15:57:18 -0400

    working

--- JS/index.js
+++ JS/index.js
@@ -7,20 +7,28 @@ for (i = 0; i < navigation.length; i++) {
     navigation[i].addEventListener('mouseout', function(event) {
       event.target.style.transform = 'scale(1)';
     })        
-}
-let logo = document.getElementsByTagName('h3');
-logo.addEventListener('mouseover', function(event) {
-    event.target.style.transform = 'scale(3)';
-});
-logo.addEventListener('mouseout', (event) => event.target.style.transform = 'scale(1)');
-// class Name{
-//     constructor(name) {
-//         this.name = name;
-//         this.expand = this.name.querySelector('')
-//     }
-// }
+};
 
-// let names = document.querySelectorAll('.name')
-// names.forEach(name => {
-//     new Name(name)
-// });
+
+class Names{
+    constructor(name) {
+        this.name = name;
+        name.addEventListener('click', (event) => {
+          if(event.target.style.transform == 'scale(2)'){
+            event.target.removeAttribute('style')
+          }else{
+            event.target.style.transform = 'scale(2)';
+          }
+        })
+    }
+}
+let tipImag = document.getElementById('tip-img')
+tipImag.addEventListener('click', (event) => {
+  if(event.target.style.transform == 'scale(2)'){
+    event.target.removeAttribute('style')
+  }else{  event.target.style.transform = 'scale(2)'} 
+})
+let names = document.querySelectorAll('.name')
+names.forEach(name => {
+    new Names(name)
+});
